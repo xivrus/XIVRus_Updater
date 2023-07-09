82c61428c521c921f62da3643ecfd6aae2d1bddc
FAR747 <far@hightgames.ru>
2023-07-09 20:00:30 +0300

    Add Create Shortcut to desktop

--- XIVRUS Updater/FirstStartPage.xaml.cs
+++ XIVRUS Updater/FirstStartPage.xaml.cs
@@ -85,6 +85,7 @@ namespace XIVRUS_Updater
 				CheckList();
 				return;
 			}
+			WinDirs.CreateAppShortcutToDesktop();
 			ConfigManager.LoadConfig();
 			mainWindow.Init();
 		}
--- XIVRUS Updater/WinDirs.cs
+++ XIVRUS Updater/WinDirs.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using IWshRuntimeLibrary;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,17 @@ namespace XIVRUS_Updater
 		public static string GetXIVLauncherConfigFolder()
 		{
 			return String.Format("{0}/XIVLauncher", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+		}
+
+		public static void CreateAppShortcutToDesktop()
+		{
+			object shDesktop = (object)"Desktop";
+			WshShell shell = new WshShell();
+			string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\XIVRUS Updater.lnk";
+			IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+			shortcut.Description = "Запустить XIVRUS Updater";
+			shortcut.TargetPath = String.Format("{0}/XIVRUSUpdater/XIVRUS Updater.exe", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+			shortcut.Save();
 		}
 	}
 }
